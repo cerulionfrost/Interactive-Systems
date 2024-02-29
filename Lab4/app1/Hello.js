4a19726d22e5691f81fca5b20cbb88030173538d
Ozi <159467429+cerulionfrost@users.noreply.github.com>
2024-02-29 19:48:57 +0200

    Lab 4 Exercises
    We did a lot but nothing worked out

---
+++ Lab4/app1/Hello.js
@@ -0,0 +1,10 @@
+'use strict';
+class Hello extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { personName:'World' };
+    }
+    render() {
+        return React.createElement('h1', null, 'Hello, ' + this.state.personName);
+    }
+}
