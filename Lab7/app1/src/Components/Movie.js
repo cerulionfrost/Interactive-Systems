598a7bd5fa9219f8873ba217e82a987ef338569d
Ozi <159467429+cerulionfrost@users.noreply.github.com>
2024-05-02 20:39:35 +0300

    Lab7
    Finished the project in general sense

--- Lab7/app1/src/Components/Movie.js
+++ Lab7/app1/src/Components/Movie.js
@@ -1,9 +1,22 @@
 import React from "react";
+import "./Movie.css"
+const ImageURL = 'https://image.tmdb.org/t/p/w500/';
 
-function Movie ()
-{
-    return ( 
-        <div>This is a Movie Component</div>
+function Movie({poster_path,title,overview,vote_average}){
+    return(
+<div className="movie">
+    <img src={ImageURL+poster_path} alt={title}></img>\
+    <div className="movie-info">
+        <h3>{title}</h3>
+        <span>{vote_average}</span>
+    </div>
+    <div className="movie-overview">
+        <h2>Overview:</h2>
+        <p>{overview}</p>
+    </div>
+
+</div>
+
     );
 }
 
