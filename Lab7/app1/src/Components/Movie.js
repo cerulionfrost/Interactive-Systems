5ad88865dc2b214f6f6d49705a8d43c5afae7ee1
Ozi <159467429+cerulionfrost@users.noreply.github.com>
2024-05-09 16:51:01 +0300

    Homework Lab7
    1) Converted the search functionality that have been added to the application into a separate component called Search component and call it inside the header of the main App.
    
    2) Added conditional rendering behavior so that when you have an empty movie image where the poster_path is null, you display a generic picture.
    
    3) Added a conditional rendering style to the voting score such that if the score is greater than 7 the color is green, if the value is between 7 and 5 the color is yellow and if it is below 5 the color of the score is red.

--- Lab7/app1/src/Components/Movie.js
+++ Lab7/app1/src/Components/Movie.js
@@ -1,23 +1,43 @@
 import React from "react";
-import "./Movie.css"
-const ImageURL = 'https://image.tmdb.org/t/p/w500/';
+import "./Movie.css";
 
-function Movie({poster_path,title,overview,vote_average}){
+const ImageURL = 'https://image.tmdb.org/t/p/w500/';
+const unavailableimage = 'https://i.imgur.com/b8D5YCz.png';
+
+function Movie({poster_path,title,overview,vote_average})
+{
+    let imageURLFinal;
+
+    
+    if (poster_path) {
+        imageURLFinal = ImageURL + poster_path;
+    } else {
+        imageURLFinal = unavailableimage;
+    }
+    let scoreColor = '';
+    if (vote_average > 7) {
+        scoreColor = 'green';
+    } else if (vote_average <= 7 && vote_average >= 5) {
+        scoreColor = 'yellow';
+    } else {
+        scoreColor = 'red';
+    }
     return(
 <div className="movie">
-    <img src={ImageURL+poster_path} alt={title}></img>\
+    <img src={imageURLFinal}alt={title}></img>
     <div className="movie-info">
         <h3>{title}</h3>
-        <span>{vote_average}</span>
+        <span className={`vote-average ${scoreColor}`}>{vote_average}</span>
     </div>
     <div className="movie-overview">
         <h2>Overview:</h2>
         <p>{overview}</p>
     </div>
-
 </div>
 
     );
 }
+
+
 
 export default Movie;
